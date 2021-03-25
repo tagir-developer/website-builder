951d3a83f6332d5478e4af42415dd3277b99ec84
Tagir Faskhutdinov (work) <tagirdjan@gmail.com>
2021-03-25 20:41:35 +0300

    Footer comleted

--- client/src/components/UI/Footer/Footer.tsx
+++ client/src/components/UI/Footer/Footer.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import './Footer.scss'
 
-
 const Footer: React.FC = () => {
 	return (
 		<div className="footer">
-			<div className="container">
+			<div className="footer__container">
 				<div className="footer__row">
 					<div className="footer__support">
 						<span className="footer__title">Поддержка</span>
 						<button className="footer__footer-button footer-button">Задать вопрос</button>
-						<button className="footer__footer-button footer-button">Отправить жалобу</button>
+						<button className="footer__footer-button footer-button">Написать жалобу</button>
 					</div>
 					<div className="footer__learning">
 						<span className="footer__title">Обучение</span>
@@ -24,8 +23,8 @@ const Footer: React.FC = () => {
 						</ul>
 					</div>
 					<div className="footer__copyright">
-						<p className="footer__cppyright-info">Все права защищены.</p>
 						<p className="footer__website">© site-generator.com</p>
+						<p className="footer__cppyright-info">Все права защищены.</p>
 					</div>
 				</div>
 			</div>
