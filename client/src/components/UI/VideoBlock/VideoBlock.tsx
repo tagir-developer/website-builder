1a7a7b9a469f44832e258cd7d3bd3c96b8687923
Tagir Faskhutdinov (home PC) <tagirdjan@gmail.com>
2021-05-10 20:43:45 +0300

    Videoinstruction page completed

---
+++ client/src/components/UI/VideoBlock/VideoBlock.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './VideoBlock.scss'
+import { NavLink } from 'react-router-dom'
+import { useCreateClassName } from '../../../hooks/createClassName.hook'
+
+interface IVideoBlock {
+	parentClass?: string
+	modClass?: string[]
+	numb: number
+	title: string
+	link: string
+}
+
+const VideoBlock: React.FC<IVideoBlock> = ({ parentClass, modClass, numb, title, link }) => {
+
+	const videoClasses = useCreateClassName('video-block', parentClass, modClass)
+
+	return (
+		<div className={videoClasses}>
+			<div className="video-block__container">
+				<div className="video-block__row">
+					<div className="video-block__title">
+						<div className="video-block__number">{numb}</div>
+						<div className="video-block__text">{title}</div>
+					</div>
+				</div>
+				<div className="video-block__row">
+					<div className="video-block__body">
+						<div className="video-block__border">
+							<div className="video-block__screen">
+								<iframe
+									className="video-block__iframe"
+									width="680"
+									height="383"
+									src={link}
+									title="YouTube video player"
+									frameBorder="0"
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+									allowFullScreen
+								></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default VideoBlock
--- client/src/pages/LearningPage/LearningPage.tsx
+++ client/src/pages/LearningPage/LearningPage.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import TopMenu from '../../components/Navigation/topMenu/TopMenu/TopMenu'
 import Footer from '../../components/UI/Footer/Footer'
+import VideoBlock from '../../components/UI/VideoBlock/VideoBlock'
+import './LearningPage.scss'
 
 //! Здесь в зависимости от авторизованности выводить разный тип меню
 
 const LearningPage: React.FC = () => {
 	return (
 		<>
-			<TopMenu menuType="back-to-main" /> 
+			<TopMenu menuType="back-to-main" />
 			<div className="content-area">
-				<h1>Видеоинструкция по работе с сервисом</h1>
+				<div className="learning-wrapper">
+					<div className="learning-header">
+						<h1 className="learning-header__title">Видеоинструкция<br /> по работе с сервисом</h1>
+					</div>
+					<div className="learning-content">
+						<VideoBlock
+							parentClass="learning-content"
+							numb={1}
+							title="Начало работы с сервисом."
+							link={"https://www.youtube.com/embed/hGIW2fDb0jg"}
+						/>
+
+						<VideoBlock
+							parentClass="learning-content"
+							numb={2}
+							title="Как создать свой первый сайт и страницу"
+							link={"https://www.youtube.com/embed/W62lE_nzwzU"}
+						/>
+					</div>
+				</div>
 			</div>
 			<Footer />
 		</>
