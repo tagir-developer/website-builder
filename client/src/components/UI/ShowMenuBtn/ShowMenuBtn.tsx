1382ff3434a18054b78008adac0c7c1b535626cd
Tagir Faskhutdinov (work) <tagirdjan@gmail.com>
2021-06-01 18:01:24 +0300

    ShowMenu CSSTransition

--- client/src/components/UI/ShowMenuBtn/ShowMenuBtn.tsx
+++ client/src/components/UI/ShowMenuBtn/ShowMenuBtn.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
-import React from 'react'
+import React, { useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import { useCreateClassName } from '../../../hooks/createClassName.hook'
 import './ShowMenuBtn.scss'
 
@@ -9,28 +10,70 @@ interface IShowMenuBtn {
 	handler?: () => void
 }
 
-const ShowMenuBtn: React.FC<IShowMenuBtn> = ({parentClass, modClass, handler}) => {
+const ShowMenuBtn: React.FC<IShowMenuBtn> = ({ parentClass, modClass, handler }) => {
 
-	// const [isOpen, isOpenHandler] = useState<boolean>(false)
+	const [isOpen, isOpenHandler] = useState<boolean>(false)
 
 	const ShowMenuBtnClasses = useCreateClassName('show-menu-btn', parentClass, modClass)
 
-	const toggleClasses = classNames({
-		'show-menu-btn__icon': true,
-	})
+	// const toggleClasses = classNames({
+	// 	'show-menu-btn__icon': true,
+	// })
 
 	const toggleHandler = () => {
-		// isOpenHandler(prev => !prev)
-		if (handler) handler()
+		isOpenHandler(prev => !prev)
+		// if (handler) handler()
 	}
 
+	const liks = [
+		{
+			title: 'Снять с публикации',
+			link: '/font-configure'
+		},
+		{
+			title: 'Изменить страницу',
+			link: '/font-configure'
+		},
+		{
+			title: 'Создать дубликат',
+			link: '/font-configure'
+		},
+		{
+			title: 'Удалить страницу',
+			link: '/font-configure'
+		},
+		{
+			title: 'Сделать главной',
+			link: '/font-configure'
+		},
+	]
+
 	return (
-		<div 
-			className={ShowMenuBtnClasses}
-			onClick={toggleHandler}
-		>
-			<div className={toggleClasses}></div>
-		</div>
+		<>
+			<div
+				className={ShowMenuBtnClasses}
+				onClick={toggleHandler}
+			>
+				<div className="show-menu-btn__menu">
+					<CSSTransition
+						in={isOpen}
+						timeout={400}
+						classNames="show-menu-btn__menu-list_show"
+						mountOnEnter
+						unmountOnExit
+					>
+						<ul className="show-menu-btn__menu-list">
+							<li className="show-menu-btn__menu-list-item" >Сделать главной</li>
+							<li className="show-menu-btn__menu-list-item" >Сделать главной</li>
+							<li className="show-menu-btn__menu-list-item" >Сделать главной</li>
+							<li className="show-menu-btn__menu-list-item" >Сделать главной</li>
+						</ul>
+					</CSSTransition>
+				</div>
+
+				<div className="show-menu-btn__icon"></div>
+			</div>
+		</>
 	)
 }
 
