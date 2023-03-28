b13a68468c46bac6c7577baa36fe1066d96ede19
Tagir Faskhutdinov <tagirdjan@gmail.com>
2023-03-28 19:01:27 +0300

    add routes

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ app.get('/', function (req, res) {
 //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 // });
 
-// app.use('/api/auth/', require('./routes/auth.routes'));
-// app.use('/api/support/', require('./routes/support.routes'));
+app.use('/api/auth/', require('./routes/auth.routes'));
+app.use('/api/support/', require('./routes/support.routes'));
 app.use('/api/projects/', require('./routes/projects.routes'));
-// app.use('/api/pages/', require('./routes/page.routes'));
-// app.use('/api/user/', require('./routes/user.routes'));
-// app.use('/api/template/', require('./routes/pageTemplate.routes'));
-// app.use('/api/blocks/', require('./routes/block.routes'));
-// app.use(errorMiddleware);
+app.use('/api/pages/', require('./routes/page.routes'));
+app.use('/api/user/', require('./routes/user.routes'));
+app.use('/api/template/', require('./routes/pageTemplate.routes'));
+app.use('/api/blocks/', require('./routes/block.routes'));
+app.use(errorMiddleware);
 
-// const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT;
 // const PORT = 80;
 
 async function start() {
