99f19f1bdb71aa5954fc28a0fbbc4da4ee390dab
Tagir Faskhutdinov <tagirdjan@gmail.com>
2023-03-29 11:36:26 +0300

    fix 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ class authController {
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: 'none',
+        // sameSite: 'none',
       });
 
       return res.json({
