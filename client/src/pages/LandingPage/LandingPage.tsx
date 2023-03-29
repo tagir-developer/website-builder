08f18ced89b2d13ef8c27f70e20db79dd04ca947
Tagir Faskhutdinov <tagirdjan@gmail.com>
2023-03-29 10:14:55 +0300

    test requests

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,65 +1,94 @@
-import React from 'react'
-import { RouteComponentProps, withRouter } from 'react-router'
-import Backdrop from '../../components/HOC/Backdrop/Backdrop'
-import PopUp from '../../components/HOC/PopUp/PopUp'
-import TopMenu from '../../components/Navigation/topMenu/TopMenu/TopMenu'
-import Button from '../../components/UI/Button/Button'
-import Footer from '../../components/UI/Footer/Footer'
-import { usePopup } from '../../hooks/usePopup.hook'
-import './LandingPage.scss'
+import axios from 'axios';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import Backdrop from '../../components/HOC/Backdrop/Backdrop';
+import PopUp from '../../components/HOC/PopUp/PopUp';
+import TopMenu from '../../components/Navigation/topMenu/TopMenu/TopMenu';
+import Button from '../../components/UI/Button/Button';
+import Footer from '../../components/UI/Footer/Footer';
+import { usePopup } from '../../hooks/usePopup.hook';
+import './LandingPage.scss';
 
 const LandingPage: React.FC<RouteComponentProps> = ({ history }) => {
+  const testPopup = usePopup(false, 'blur');
 
-	const testPopup = usePopup(false, 'blur')
+  const testRequestOne = (): void => {
+    axios.get('https://website-builder-two-fawn.vercel.app/api/test/test');
+  };
+  const testRequestTwo = (): void => {
+    axios.get(
+      'website-builder-client-lhroo6jve-tagir-developer.vercel.app/api/test/test'
+    );
+  };
+  const testRequestThree = (): void => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json));
+  };
 
-	return (
-		<>
-			{/* <ScrollTo value={testPopup.scroll} dep={testPopup.isOpen} /> */}
+  return (
+    <>
+      {/* <ScrollTo value={testPopup.scroll} dep={testPopup.isOpen} /> */}
 
-			<PopUp {...testPopup.popupProps} withTitle="Основные настройки">
-				{/* <BasicSettings handler={() => { }} /> */}
-			</PopUp>
+      <PopUp {...testPopup.popupProps} withTitle='Основные настройки'>
+        {/* <BasicSettings handler={() => { }} /> */}
+      </PopUp>
 
-			<Backdrop {...testPopup.backdropProps} >
-
-				<TopMenu menuType="main" />
-				{/* <div className="content-area" style={testPopup.isOpen ? {marginTop: `-${testPopup.scroll - 60}px`} : undefined} > */}
-				<div className="content-area">
-					<div className="landing-wrapper">
-						<div className="landing-header">
-							<div className="landing-header__container">
-								<div className="landing-header__text-container">
-									<h1 className="landing-header__heading">Хотите создать сайт-визитку для Instagram?</h1>
-									<Button parentClass="landing-header" handler={() => history.push('/registration')} >
+      <Backdrop {...testPopup.backdropProps}>
+        <TopMenu menuType='main' />
+        {/* <div className="content-area" style={testPopup.isOpen ? {marginTop: `-${testPopup.scroll - 60}px`} : undefined} > */}
+        <div className='content-area'>
+          <div className='landing-wrapper'>
+            <div className='landing-header'>
+              <div className='landing-header__container'>
+                <div className='landing-header__text-container'>
+                  <h1 className='landing-header__heading'>
+                    Хотите создать сайт-визитку для Instagram?
+                  </h1>
+                  <Button parentClass='landing-header' handler={testRequestOne}>
+                    Тестовый запрос 1
+                  </Button>
+                  <Button parentClass='landing-header' handler={testRequestTwo}>
+                    Тестовый запрос 2
+                  </Button>
+                  <Button
+                    parentClass='landing-header'
+                    handler={testRequestThree}
+                  >
+                    Тестовый запрос 3
+                  </Button>
+                  {/* <Button parentClass="landing-header" handler={() => history.push('/registration')} >
 										Создать сайт
-									</Button>
-								</div>
-								<div className="landing-header__image-container">
-									<div className="landing-header__image"></div>
-								</div>
-							</div>
-						</div>
-						<div className="landing-blocks">
-							<div className="landing-blocks__container">
-								<div className="landing-blocks__blocks-wrapper">
-									<div className="landing-blocks__block-light">Блок 2</div>
-									<div className="landing-blocks__block-dark">Блок 3</div>
-									<div className="landing-blocks__block-light">Блок 4</div>
-								</div>
-							</div>
-						</div>
-						<div className="landing-final">
-							<Button parentClass="landing-final" handler={() => history.push('/registration')} >
-								Создать сайт
-							</Button>
-						</div>
-					</div>
-				</div>
-				<Footer />
+									</Button> */}
+                </div>
+                <div className='landing-header__image-container'>
+                  <div className='landing-header__image'></div>
+                </div>
+              </div>
+            </div>
+            <div className='landing-blocks'>
+              <div className='landing-blocks__container'>
+                <div className='landing-blocks__blocks-wrapper'>
+                  <div className='landing-blocks__block-light'>Блок 2</div>
+                  <div className='landing-blocks__block-dark'>Блок 3</div>
+                  <div className='landing-blocks__block-light'>Блок 4</div>
+                </div>
+              </div>
+            </div>
+            <div className='landing-final'>
+              <Button
+                parentClass='landing-final'
+                handler={() => history.push('/registration')}
+              >
+                Создать сайт
+              </Button>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </Backdrop>
+    </>
+  );
+};
 
-			</Backdrop>
-		</>
-	)
-}
-
-export default withRouter(LandingPage)
+export default withRouter(LandingPage);
